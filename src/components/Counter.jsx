46c2bdefa2ffb3c7d1e2b983ab80200d484f7594
ivan <eshchin.id@gmail.com>
2023-01-23 15:43:21 +0300

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+
+const Counter = () => {
+    const [likes, setLikes] = useState(0);
+    const [comment, setComment] = useState('text')
+
+    const inc = () => {setLikes(likes+1)};
+    const dec = () => setLikes(likes-1);
+
+    return (
+        <div>
+            <h1>{likes}</h1>
+            <button onClick={inc}>Увеличить</button>
+            <button onClick={dec}>Уменьшить</button>
+            <h1>{comment}</h1>
+            <input
+                type='text'
+                value={comment}
+                onChange={event => setComment(event.target.value)}
+            />
+        </div>
+    );
+};
+
+export default Counter;
